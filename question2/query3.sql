e6c0a59ec2aba9dfec4145438d93aa4cf5ddf403
Sashank Tatavolu <84382110+Sashanktatavolu@users.noreply.github.com>
2023-01-01 22:10:40 +0530

    question2

---
+++ question2/query3.sql
@@ -0,0 +1,7 @@
+INSERT INTO FootballVenue(venue_id int,venue_name varchar(100),city_id int,capacity int)
+
+VALUES (11,'devils',01,10000),(12,'RoyalchallengersBanglore',02,20000),
+(13,'ChennaiSuperkings',03,30000),(14,'KolkataKnightriders',04,40000),
+(15,'DelhiKnights',05,5000),(16,'HaryanaSteelers',06,60000),
+(17,'Delhidaredevils',07,70000),(18,'Pradesh',08,80000),
+(19,'KeralaWarriors',09,90000),(110,'Goakings',10,100000);
